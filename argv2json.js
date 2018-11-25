b44ecca1ccc431f2a9aa768c4933b54b77ec2300
Denislav Davidov <denislav@mean.studio>
2018-11-25 18:56:36 +0000

    initial commit

---
+++ argv2json.js
@@ -0,0 +1,23 @@
+module.exports = class argv2json {
+  parse(parseArrays=true) {
+    let args = {};
+    args.runtime = process.argv[0];
+    args.initiator = process.argv[1];
+    let argv = process.argv.splice(2);
+    console.log(argv);
+    argv.forEach(element => {
+      let currentElement = element.split('=');
+      if (currentElement[1]) {
+          if(parseArrays){
+            args[currentElement[0]]=currentElement[1].split(',')
+          }else{
+
+              args[currentElement[0]] = currentElement[1];
+          }
+      } else {
+        args[currentElement[0]] = true;
+      }
+    });
+    return args;
+  }
+};
--- index.js
+++
@@ -1,3 +0,0 @@
-argv2json = require('./argv2json');
-arg = new argv2json();
-console.log(arg.parse(false))
